388a51f274e1536f0bdf7e8d78f411bbf878c211
doanvanchau13399 <doanvanchau13399@gmail.com>
2020-08-12 15:03:38 +0700

    fix ui forgot password

--- src/Component/ForgotPassword/index.js
+++ src/Component/ForgotPassword/index.js
@@ -1,35 +1,46 @@
-import React from "react";
-import "./style.scss";
-import { forgotpassword } from "../../Redux/Action/userAction";
-import { toast } from "react-toastify";
+import React from 'react';
+import './style.scss';
+import { forgotpassword } from '../../Redux/Action/userAction';
+import { toast } from 'react-toastify';
 
 function ForgotPassword() {
   const handleSubmit = (event) => {
     event.preventDefault();
     const formData = new FormData(event.target);
     const data = {
-      email: formData.get("email"),
+      email: formData.get('email'),
     };
     forgotpassword(data)
       .then((res) => {
         if (res.status === 200) {
-          toast.success("Send successfully");
+          toast.success('Send successfully');
         }
       })
       .catch((err) => {
-        toast.error("Send fail");
+        toast.error('Send fail');
       });
   };
   return (
-    <form onSubmit={handleSubmit}> 
-      <div className="form-input">
-        <label for="email" className="label">
-          Email
-        </label>
-        <input id="email" name="email" className="input" required />
-        <button type="submit">Submit</button>
+    <div className="main-forget">
+      <div className="header-forget">
+        <h1>Forgot your password?</h1>
+        <hr></hr>
+        <h2>Enter your email address to reset your password</h2>
       </div>
-    </form>
+      <div className="content-forget">
+        <form onSubmit={handleSubmit} className="form">
+          <label for="email">Email</label>
+          <input
+            id="email"
+            name="email"
+            className="form-input"
+            placeholder="Enter your email address"
+            required
+          />
+          <input type="submit" className="button form-submit" value="Submit" />
+        </form>
+      </div>
+    </div>
   );
 }
 
