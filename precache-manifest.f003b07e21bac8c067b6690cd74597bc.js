65d3e3957489a4201bcccd343bb9ab9af8d3359e
Hieu Hoang <megamind.ft@gmail.com>
2020-08-24 20:49:26 +0700

    Updates

---
+++ precache-manifest.f003b07e21bac8c067b6690cd74597bc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a57d016c7cca90a65c3eb69f4db88d19",
+    "url": "/web2-2020/index.html"
+  },
+  {
+    "revision": "6a46b0b34b43e96b43f7",
+    "url": "/web2-2020/static/css/2.37865b2a.chunk.css"
+  },
+  {
+    "revision": "a64e678ec18ad2fbd5b7",
+    "url": "/web2-2020/static/css/main.e1169e61.chunk.css"
+  },
+  {
+    "revision": "6a46b0b34b43e96b43f7",
+    "url": "/web2-2020/static/js/2.0e5a6a05.chunk.js"
+  },
+  {
+    "revision": "47443401be3cfeb14f0c199b46ba616c",
+    "url": "/web2-2020/static/js/2.0e5a6a05.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a64e678ec18ad2fbd5b7",
+    "url": "/web2-2020/static/js/main.af72cd1e.chunk.js"
+  },
+  {
+    "revision": "d7d28da0e8bd1ce63ab3",
+    "url": "/web2-2020/static/js/runtime-main.f070835a.js"
+  }
+]);
